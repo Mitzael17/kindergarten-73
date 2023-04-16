5d0678356c8202796490cc502aff60becc9b9fd4
Mitzael17 <misha.fokin.04@mail.ru>
2023-04-16 23:02:08 +0300

    Features block is ready

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,7 @@ import {createFooterSpoilers, createSpoilers} from "./modules/createSpoilers.js"
 import {createPasswordFields} from "./modules/createPasswordFields.js";
 import {createBurger} from "./modules/createBurger.js";
 import {transferContactsInHeader} from "./modules/transferContactsInHeader.js";
+import {createDataWithContentBlocks} from "./modules/createDataWithContentBlocks.js";
 
 createSpoilers();
 createFooterSpoilers();
@@ -10,3 +11,5 @@ createPasswordFields();
 
 const handlerMenuResize = createBurger();
 transferContactsInHeader(handlerMenuResize);
+
+createDataWithContentBlocks();
---
+++ src/scripts/modules/createDataWithContentBlocks.js
@@ -0,0 +1,52 @@
+export function createDataWithContentBlocks() {
+
+    const blocks = document.querySelectorAll('[data-width-content="true"]');
+    const items = [];
+
+    window.addEventListener('resize', handlerResize)
+
+    blocks.forEach( block => {
+
+        const span = block.firstElementChild;
+
+        if(!span) return;
+
+        items.push({
+            spanNode: span,
+            blocNode: block,
+            prevSize: span.offsetWidth
+        });
+
+    })
+
+    handlerResize();
+
+    function handlerResize() {
+
+        items.forEach( item => {
+
+            item.blocNode.style.maxWidth = 'none';
+
+            const resize = setTimeout( () => {
+
+                if(item.spanNode.offsetWidth === item.prevSize) {
+
+                    item.blocNode.style.maxWidth = item.prevSize + 2 + 'px';
+
+                    return;
+
+                }
+
+                item.blocNode.style.maxWidth = item.spanNode.offsetWidth + 2 + 'px';
+
+                item.prevSize = item.spanNode.offsetWidth;
+
+            }, 100)
+
+            window.addEventListener('resize', () => clearTimeout(resize), {once: true});
+
+        })
+
+    }
+
+}
